2c8df95b573b0336d36d1ad1398299cb2f7367bd
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-07-27 17:13:41 +0100

    - add packages - add server - serve static files

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+require('dotenv').config()
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('client'))
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
