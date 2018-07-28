f5edf839aa72a65c8ea8738326cc9bbbc6a66e7d
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-07-28 16:04:38 +0100

    - add js for modal

--- client/index.js
+++ client/index.js
@@ -1,1 +1,18 @@
-// alert('here')
+const modal = document.querySelector('#modal');
+const closeBtn = document.querySelector('.close');
+const downloadBtn = document.querySelector('.download-btn');
+
+downloadBtn.onclick = () => {
+  modal.style.display = "block"
+}
+
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+}
+
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
