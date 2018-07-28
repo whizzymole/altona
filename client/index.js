edbd02fb95c51e88bfcbc8a12c3099e1943cde60
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-07-28 16:15:20 +0100

    - restyle modal

--- client/index.js
+++ client/index.js
@@ -1,11 +1,12 @@
 const modal = document.querySelector('#modal');
 const closeBtn = document.querySelector('.close');
 const downloadBtn = document.querySelector('.download-btn');
+const form = document.querySelector('#sub-form');
+const email = document.querySelector('#email');
 
 downloadBtn.onclick = () => {
   modal.style.display = "block"
 }
-
 
 closeBtn.onclick = () => {
   modal.style.display = "none";
@@ -15,4 +16,9 @@ window.onclick = (event) => {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  console.log(email.value)
 }
